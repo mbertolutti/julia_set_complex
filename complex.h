060ffb40dee1abcb742d012892d23575fa88be99
mbertolutti <mbertolutti@localhost>
2012-11-21 20:12:40 +0100

    Initial commit

---
+++ complex.h
@@ -0,0 +1,36 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <cmath>
+
+class complex
+{
+public:
+	complex(double real, double imag)
+		: real(real), imag(imag)
+	{
+	}
+
+	double abs()
+	{
+		return std::sqrt(real * real + imag * imag);
+	}
+
+	complex operator+ (complex other)
+	{
+		complex result(real + other.real, imag + other.imag);
+		return result;
+	}
+
+	complex operator* (complex other)
+	{
+		// (a+bi)*(c+di) =a*c-bd+(a*d+b*c)i
+		complex result(real * other.real - imag * other.imag, real * other.imag + imag * other.real);
+		return result;
+	}
+
+private:
+	double real, imag;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "complex.h"
+
+
+double julia(complex c)
+{
+	complex seed(-0.8, 0.156);
+	double i = 0, n = 200;;
+
+	do
+	{
+		c = c * c + seed;
+	} while (++i < n && c.abs() < 5);
+
+	return i / (n + 1);
+}
+
+int main()
+{
+	const char density[] = { ' ', '.', ',', '-', '~', '*', '+', '#', '$' };
+	double width = 260, height = 80;
+
+	for (std::size_t i = 0; i < height; ++i)
+	{
+		for (std::size_t j = 0; j < width; ++j)
+		{
+			complex pos(2 - 4 * j / width, 2 * i / height - 1);
+			std::size_t symbol = julia(pos) * sizeof(density);
+			std::cout << density[symbol];
+		}
+
+		std::cout << '\n';
+	}
+
+	std::cout << "Press any key to continue" << std::endl;
+	std::cin.get();
+}
